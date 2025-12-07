e2325d1c73c07d042b82dd9a6b0af11b04011991
Naga-Bits <2023aa05111@wilp.bits-pilani.ac.in>
2025-12-07 13:09:34 +0800

    Enable default SSH access via Cloud IAP

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,7 +27,8 @@ variable "machine_type" {
 variable "allowed_ssh_source_ranges" {
   type        = list(string)
   description = "List of CIDR blocks allowed to reach SSH (port 22). Avoid 0.0.0.0/0 for production."
-  default     = []
+  # Default allows SSH only through Cloud IAP TCP forwarding (35.235.240.0/20)
+  default     = ["35.235.240.0/20"]
 
   validation {
     condition     = var.allowed_ssh_worldwide_override || !contains(var.allowed_ssh_source_ranges, "0.0.0.0/0")
